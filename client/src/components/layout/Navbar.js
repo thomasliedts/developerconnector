6a48638661bdb63da337eb9483ff0ae6d98b1f35
Solid-Wolf <thomasliedts@gmail.com>
2020-06-02 13:41:27 +0200

    Display comment from DB to client

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,11 +8,12 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>
-          {' '}
-          <i className='fas fa-user'></i>{' '}
-          <span className='hide-sm'>Developers</span>
-        </Link>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           {' '}
           <i className='fas fa-user'></i>{' '}
